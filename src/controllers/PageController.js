b0d5f646b21a326d8a5aa6064fc41a0c52dc93f6
smitsleon <leon.smits@student.arteveldehs.be>
2025-02-18 12:56:39 +0100

    header and footer added with ejs file

--- src/controllers/PageController.js
+++ src/controllers/PageController.js
@@ -1,9 +1,11 @@
 import { person } from "../data/data.js";
+import { navItems } from "../data/data.js";
 
 export const home = (req, res)=> {
     res.render("pages/home", {
         title: "Dinosaurs around the world",
         content: "Welcome to the world of dinosaurs",
+        navItems,
     })
 };
 
@@ -11,7 +13,8 @@ export const about = (req, res)=> {
     res.render("pages/default", {
         title: "About us",
         content: "We are a group of dinosaurs experts",
-        team: ["T-rex", "Velociraptor", "Stegosaurus", "<strong>Premium</strong> Diplodocus"]
+        team: ["T-rex", "Velociraptor", "Stegosaurus", "<strong>Premium</strong> Diplodocus"],
+        navItems,
     })
 };
 
@@ -20,6 +23,7 @@ export const contact = (req, res)=> {
         title: "Contact",
         content: "Contact us at 1234567890",
         person,
+        navItems,
     })
 };
 
@@ -28,5 +32,6 @@ export const privacy = (req, res)=> {
         title: "Privacy Policy",
         content: "Dinosaurs are protective of their privacy",
         team: false,
+        navItems,
     })
 };
