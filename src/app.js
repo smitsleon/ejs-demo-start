a4e5d9f44abb2cd5e28be63ed5be6eb5e35648bf
smitsleon <leon.smits@student.arteveldehs.be>
2025-02-18 14:20:40 +0100

    last commit on 18/02

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 // import statements
 import express from "express";
+import expressLayouts from "express-ejs-layouts";
 import path from "path";
 
 import {home, about, contact, privacy} from "./controllers/PageController.js";
@@ -7,7 +8,11 @@ import {home, about, contact, privacy} from "./controllers/PageController.js";
 // create an instance of express
 const app = express();
 
+// use express layouts
+app.use(expressLayouts)
+// set the view enigine to ejs
 app.set("view engine", "ejs");
+app.set("layout", "layouts/main")
 app.set("views", path.resolve("src", "views"));
 
 // serve static files from the public folder
@@ -19,6 +24,12 @@ app.get("/", home)
 app.get("/about", about)
 app.get("/contact", contact)
 app.get("/privacy", privacy)
+
+app.get("*", (req, res) => {
+  res.status(404).render("errors/404", {
+    layout: "layouts/error",
+  });
+});
 
 // GET route to serve the index.html file
 app.get("/", (req, res) => {
