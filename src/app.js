e976aab63c0e34045cbf27f00ef4579f264fdf9b
smitsleon <leon.smits@student.arteveldehs.be>
2025-02-18 09:35:36 +0100

    first commits

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 // import statements
 import express from "express";
 import path from "path";
+import { title } from "process";
 
 // create an instance of express
 const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("src", "views"));
 
 // serve static files from the public folder
 // they can be accessed from the root of the site (e.g. /assets/images/dino_07.png) 🦕
@@ -11,7 +15,11 @@ app.use(express.static("public"));
 
 // GET route to serve the index.html file
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve("src", "views", "index.html"));
+  res.render("home", {
+    title: "Dinosaurs are awesome",
+    content: 
+      "Dinosaurs are cool creatures",
+  });
 });
 
 // start the server, listen on port defined in .env file
